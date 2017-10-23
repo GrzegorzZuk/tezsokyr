c22b5bb6890926e9da818bc24c82965b7746f9d3
SuliusB <32463368+SuliusB@users.noreply.github.com>
2017-10-23 12:23:49 +0200

    Add files via upload

---
+++ _ReboundProject/Scripts/BulletController2Script.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController2Script : MonoBehaviour {
+
+    public LayerMask collisionMask;
+
+    //public Transform blade;
+    public float _speed;
+    //public float rotSpeed;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        //Move the bullet farwards
+        transform.Translate(Vector3.forward * Time.deltaTime * _speed);
+        //Rotate the blade
+        //blade.Rotate(Vector3.up * Time.deltaTime * rotSpeed);
+
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+		
+        if (Physics.Raycast(ray, out hit, Time.deltaTime * _speed , collisionMask))
+        {
+            Vector3 reflectDirection = Vector3.Reflect(ray.direction, hit.normal);
+            float rot = 90 - Mathf.Atan2(reflectDirection.z, reflectDirection.x) * Mathf.Rad2Deg;
+            transform.eulerAngles = new Vector3(0, rot, 0);
+        }
+	}
+}
---
+++ _ReboundProject/Scripts/GunControllerScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunControllerScript : MonoBehaviour {
+
+    public bool _isFiring;
+    public BulletControllerScript bullet;
+    public float speedBullet;
+
+    public float timeBetweenShots;
+    private float shotCounter;
+
+    public Transform firePoint;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        if (_isFiring)
+        {
+            shotCounter -= Time.deltaTime;
+            if(shotCounter <= 0)
+            {
+                shotCounter = timeBetweenShots;
+                BulletControllerScript newBullet =  Instantiate(bullet, firePoint.position, firePoint.rotation) as BulletControllerScript;
+                newBullet._speed = speedBullet;
+            }
+        }
+        else
+        {
+            shotCounter = 0;
+        }
+	}
+}
